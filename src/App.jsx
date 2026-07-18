51282085026660f355fcb0e605478171db0da673
보성 <hps_in@naver.com>
2026-07-18 15:43:47 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -178,7 +178,7 @@ export default function App() {
               transform: "translateX(-50%)",
               width: "100%",
               maxWidth: MOBILE_SHELL_WIDTH,
-              padding: "12px 14px 0",
+              padding: "12px 14px 30px",
               overflow: "hidden",
               zIndex: 1,
             }}
