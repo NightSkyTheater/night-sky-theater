6a491ba79cbf055c7a68eb8f83f99bc085b63c5c
보성 <hps_in@naver.com>
2026-09-06 09:29:16 +0900

    update

--- src/components/HomeTab.jsx
+++ src/components/HomeTab.jsx
@@ -36,8 +36,8 @@ export default function HomeTab({ setTab }) {
             <h1>모든 밤에는<br /><em>남겨야 할</em><br />노래가 있다.</h1>
             <p>밤하늘극장은 청춘과 사랑, 삶과 죽음에 관한 이야기를 음악으로 풀어내는 버츄얼 인디 밴드입니다. 한 곡의 노래에서 시작된 감정과 서사를 가사와 캐릭터, 비주얼, 영상으로 확장하며 밤하늘극장만의 세계를 만들어갑니다.</p>
             <div className="hero-actions">
-              <button className="btn primary" onClick={() => setTab("music")}>EXPLORE RELEASES <ArrowRight size={16} /></button>
-              <button className="btn ghost" onClick={() => setTab("about")}>ABOUT THE LABEL</button>
+              <button className="btn primary" onClick={() => setTab("music")}>음악 둘러보기 <ArrowRight size={16} /></button>
+              <button className="btn ghost" onClick={() => setTab("about")}>밤하늘극장 소개</button>
             </div>
           </div>
           <div className="hero-index">
