624587480366b076d9567e5b38b716f53e197226
보성 <hps_in@naver.com>
2026-09-06 19:01:39 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 ﻿import React from "react";
+
 import MainSite from "./MainSite";
 import MaintenancePage from "./components/MaintenancePage";
 
 export default function App() {
-  const path = window.location.pathname.replace(/\/+$/, "");
-  const isPreview = path === "/nst-preview-2026";
+  const MAINTENANCE_MODE = false;
 
-  return isPreview ? <MainSite /> : <MaintenancePage />;
+  if (MAINTENANCE_MODE) {
+    return <MaintenancePage />;
+  }
+
+  return <MainSite />;
 }
